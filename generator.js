1306ddbf97f40a50260b46e1662055d502655211
m19e <nexus.me7290@gmail.com>
2019-10-16 11:21:18 +0900

    迷路の総グリッド数を数える関数を追加

--- generator.js
+++ generator.js
@@ -1,3 +1,13 @@
-(stdin => {
-	
-})(require("fs").readFileSync("/dev/stdin", "utf8"))
+// (stdin => {
+// 	const input = stdin
+//  console.log(input)
+// })(require('fs').readFileSync('/dev/stdin', 'utf8'))
+
+const text = 'この文章は迷路のようになっていて、少し読み辛いかもしれません。もしこの文章が読めたなら、あなたには迷路の才能があると思います。ここまで来たらあと少しです。通らなかった文字を上から読んで下さい。'
+const cipher = 'めいろ'
+
+countMazeGrid = (text, cipher) => {
+	return [...(text + cipher)].length
+}
+
+console.log(countMazeGrid(text, cipher))

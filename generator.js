f6efdd366ca72745137a13d6321f1b57777ab0f5
m19e <nexus.me7290@gmail.com>
2019-10-16 12:38:43 +0900

    総グリッド数を素因数分解する関数を追加

--- generator.js
+++ generator.js
@@ -6,8 +6,30 @@
 const text = 'この文章は迷路のようになっていて、少し読み辛いかもしれません。もしこの文章が読めたなら、あなたには迷路の才能があると思います。ここまで来たらあと少しです。通らなかった文字を上から読んで下さい。'
 const cipher = 'めいろ'
 
-countMazeGrid = (text, cipher) => {
+const countMazeGrid = (text, cipher) => {
 	return [...(text + cipher)].length
 }
 
-console.log(countMazeGrid(text, cipher))
+const primeFactorization = n => {
+	let s = Math.floor(Math.sqrt(n))
+	let r = 0
+	let result = []
+
+	for (let i = 2;i <= s;i++) {
+		if ((n % i) == 0) {
+			r = 0
+            do {
+				r++
+                n = n / i
+			} while ((n % i) == 0)
+			result.push({num:i, r:r})
+		}
+	}
+	
+	if (n > s) {
+		result.push({num:n, r:1})
+	}
+	return result
+}
+
+console.log(primeFactorization(countMazeGrid(text, cipher)))

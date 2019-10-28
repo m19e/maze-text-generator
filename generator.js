b4c7f760479905fa56fc19285bea674814dbab4c
m19e <nexus.me7290@gmail.com>
2019-10-28 11:59:41 +0900

    迷路をオブジェクトにした
    横一列が一度のfillGridで埋められてしまう問題を解決

--- generator.js
+++ generator.js
@@ -11,6 +11,43 @@ const countMazeGrid = (text, cipher) => {
   return [...(text + cipher)].length;
 };
 
+var tail_recursion = function(func) {
+    var is_first = true;
+    var cont = {};
+    var saved_args;
+    var _in = function() {
+        if (is_first) {
+            var args = arguments;
+            while (true) {
+                is_first = false;
+                var result = func.apply(null, args);
+                if (result === cont) {
+                    args = saved_args;
+                    is_first = true;
+
+                } else {
+                    is_first = true;
+                    return result;
+                }
+            }
+        } else {
+            saved_args = arguments;
+            return cont;
+        }
+    };
+    return _in;
+};
+
+const trampoline = (fn) => {
+  return (...args) => {
+    let result = fn(...args);
+    while (typeof result === 'function') {
+      result = result();
+    }
+    return result;
+  };
+}
+
 const primeFactorization = n => {
   let s = Math.floor(Math.sqrt(n));
   let r = 0;
@@ -99,8 +136,19 @@ let pairedArray = uniq([2, 2, 5, 7].permutation(), [].join).map(arr => {
   return (sortAsc(pair));
 });
 
-// console.log(pairedArray);
-// console.log(uniq(pairedArray, [].join));
+const shuffle = arr => {
+  let i, j, temp;
+  arr = arr.slice();
+  i = arr.length;
+  if (i === 0) return arr
+  while (--i) {
+    j = Math.floor(Math.random() * (i + 1));
+    temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+  }
+  return arr;
+};
 
 const selectedAspectPairs = arrs => {
   let obj = {}
@@ -109,15 +157,14 @@ const selectedAspectPairs = arrs => {
 }
 
 const calculateMazeAspect = arrs => {
-  let odd, mid
   let options = []
-  // odd = arrs[0].length % 2 !== 0 ? true : false
+  if (arrs.length < 3) return arrs
   for (arr of arrs) {
     let option = []
     while (arr.length !== 0) {
       if (arr.length === 1) {
-         option = sortAsc(option)
-         option[0] = option[0] * arr.pop()
+        option = sortAsc(option)
+        option[0] = option[0] * arr.pop()
       } else {
         option.push(arr.pop() * arr.pop())
       }
@@ -132,9 +179,18 @@ const generateEmptyArray = arr => {
 };
 
 const fillArray = arr => {
-	return arr.map(x => new Array(x.length).fill('ほ'))
+  // before
+  // fillにそのままオブジェクトを渡してはダメです
+  // return arr.map(x => new Array(x.length).fill({char: 'ほ', blacklist: []}))
+
+  // after
+  // fillで埋めたあとにmapでオブジェクトで埋めてあげるといい感じ
+	return arr.map(x => [...Array(x.length)].map(_=>({char: 'ほ', blacklist: []})))
 };
 
+const generateFilledArray = arr => {
+  return [...Array(arr[1])].map(_=>[...Array(arr[0])].map(_=>({char: 'ほ', blacklist: []})))
+}
 
 const moveUp = position => {
   return {
@@ -157,7 +213,6 @@ const moveDown = position => {
   }
 }
 
-
 const moveRight = position => {
   return {
     x: position.x + 1,
@@ -174,33 +229,68 @@ const searchAround = (maze, position) => {
   ].filter( dir => {
     if (!maze[dir.y]) return false
     if (!maze[dir.y][dir.x]) return false
-    return maze[dir.y][dir.x] === 'ほ'
+    return !(maze[position.y][position.x]['blacklist'].map( x => JSON.stringify(x)).includes(JSON.stringify(dir))) && maze[dir.y][dir.x]['char'] === 'ほ'
+    // return maze[dir.y][dir.x]['char'] === 'ほ'
   })
 }
 
 const selectDirection = (maze, position) => {
-  return searchAround(maze, position).pop()
+  return shuffle(searchAround(maze, position)).pop()
 }
 
-const recursiveFillMaze = (maze, position = { x: 0, y: 0 }) => {
-  return maze
+// const testText = '黒猫列車はお客様第一'
+const testText = '銀河を繋ぐ黒猫列車、本日ひとりめのお客様がご乗車ですっ'
+const testArray = [...testText]
+
+const resetArgs = () => {
+  console.log('リセットします');
+  return fillAGrid(result, [...text], [] ,{ x: 0, y: 0 })
 }
 
-// let testText = '黒猫列車はお客様第一'
-let testText = '銀河を繋ぐ黒猫列車、本日ひとりめのお客様がご乗車ですっ'
-let testArray = [...testText]
+const goBack = (maze, will, position, memory) => {
+  console.log('もどります');
+  if (memory.length === 0) display(maze);
+  console.log(memory);
+  const memo = memory.pop()
+  console.log(memory, memo);
+  maze[memo.y][memo.x]['blacklist'].push(position)
+  will.unshift(maze[position.y][position.x]['char'])
+  maze[position.y][position.x]['char'] = 'ほ'
+  return fillAGrid(maze, will, memo)
+}
 
-const fillAGrid = (maze, road, position = { x: 0, y: 0 }) => {
-  if (road.length === 0) {
-    return maze
-  } else {
-    maze[position.y][position.x] = road.shift()
-    return fillAGrid(maze, road, selectDirection(maze, position))
+const fillAGrid = (maze, will, position = { x: 0, y: 0 }, memory = []) => {
+  try {
+    // if (will.length === 0) return maze
+    if (will.length === 0) {
+      return maze
+    }
+    maze[position.y][position.x]['char'] = will.shift()
+    
+    // ここで一歩もどってブラックリストを作りたい
+    if (searchAround(maze, position).length === 0) return goBack(maze, will, position, memory)
+    // if (searchAround(maze, position).length === 0) return trampoline(resetArgs())
+    memory.push(position)
+    if (memory.length === 0) display(maze);
+    return fillAGrid(maze, will, selectDirection(maze, position), memory)
+  } catch (e) {
+    console.error(e);
   }
 }
 
+// fillAGrid = tail_recursion(fillAGrid)
+
 const toMaze = arr => {
 	return arr.map(x => x.join(''))
+}
+
+const display = array => {
+  array.forEach( (arr,i) => {
+    console.log(
+      arr.map((x,i) => x.char).join(''),
+      i
+    );
+  })
 }
 
 // console.log((getPrimeFactors(countMazeGrid(text, cipher)).permutation()))
@@ -212,16 +302,33 @@ const toMaze = arr => {
 //   [...new Array(3).keys()].map(i => { return i + 9 }).join(''),
 // );
 
-const aspect = sortAsc(selectedAspectPairs(calculateMazeAspect(uniq(getPrimeFactors(
-  countMazeGrid(text, cipher)
-  // 108
-).permutation(), [].join))))
+// const aspect = sortAsc(selectedAspectPairs(calculateMazeAspect(uniq(getPrimeFactors(
+//   // countMazeGrid(text, cipher)
+//   countMazeGrid(testText, cipher)
+// ).permutation(), [].join))))
 
-const result = fillArray(generateEmptyArray(aspect))
+// const result = fillArray(generateEmptyArray(aspect))
 // console.log(result[0]);
-const resultCopy = result.map(x => x)
-const nyan = fillAGrid(resultCopy, testArray)
 
-toMaze(nyan).forEach( (x,i) => {
-	console.log(x, i+1);
+const result = fillArray(generateEmptyArray(sortAsc(
+  // selectedAspectPairs(calculateMazeAspect(uniq(getPrimeFactors(
+  // countMazeGrid(text, cipher)
+  // ).permutation(), [].join)))
+  [10,10]
+)))
+
+const nyan = fillAGrid(result, testArray)
+
+// console.log(nyan);
+
+// console.log(typeof nyan);
+nyan.forEach( (arr,i) => {
+	console.log(
+    arr.map((x,i) => x.char).join(''),
+    // i
+  );
 })
+
+// console.log(
+//   generateFilledArray([10,10])
+// );
